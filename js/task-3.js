8545dd36f6597e7cd689ac83418c49263e001327
Bohdan Babych <babychbohdan0@gmail.com>
2024-01-17 23:59:22 +0100

    homework number 6

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,33 @@
-function checkForSpam(message){
-    let lowerMessage = message.toLowerCase();
-  if (lowerMessage.includes("spam") || lowerMessage.includes("sale"))
-  {
-    return `true`
+class StringBuilder {
+  #value;
+  
+  constructor(value) {
+      this.#value = value;
   }
-  else {return `false
-  `}
+
+  getValue() {
+      return this.#value;
+  }
+
+  padEnd(str) {
+      this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+      this.#value = str + this.#value; 
+  }
+
+  padBoth(str) {
+      this.#value = (str + this.#value) + str;
+  }
+
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
